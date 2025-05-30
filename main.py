e821073d99cee6ec38fd9dc6551a2776c3bfe19d
chuka <emry390@gmail.com>
2025-05-30 09:22:04 +0100

    Fix error of non-existing places and wrong temperature values displayed.

--- main.py
+++ main.py
@@ -11,22 +11,25 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} day(s) in {place}")
 
 if place:
-    # Get temperature/sky values
-    filtered_content = get_data(place=place, forecast_days=days)
+    try:
+        # Get temperature/sky values
+        filtered_content = get_data(place=place, forecast_days=days)
 
-    if option == "Temperature":
-        temperatures = [data["main"]["temp"] for data in filtered_content]
-        dates = [data["dt_txt"] for data in filtered_content]
-        # Plotting graph
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperatures"})
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [data["main"]["temp"] /10 for data in filtered_content]
+            dates = [data["dt_txt"] for data in filtered_content]
+            # Plotting graph
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperatures(C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        images = {"Clear": "sky_images/clear.png", "Clouds": "sky_images/cloud.png",
-                  "Rain": "sky_images/rain.png", "Snow": "sky_images/snow.png"}
-        sky = [sky_data["weather"][0]["main"] for sky_data in filtered_content]
-        # Translating data from sky and images
-        image_paths = [images[condition] for condition in sky]
-        st.image(image_paths, width=100)
+        if option == "Sky":
+            images = {"Clear": "sky_images/clear.png", "Clouds": "sky_images/cloud.png",
+                      "Rain": "sky_images/rain.png", "Snow": "sky_images/snow.png"}
+            sky = [sky_data["weather"][0]["main"] for sky_data in filtered_content]
+            # Translating data from sky and images
+            image_paths = [images[condition] for condition in sky]
+            st.image(image_paths, width=100)
+    except KeyError:
+        st.error("Place does not exist!!")
 
 
